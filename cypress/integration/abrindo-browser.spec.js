debc6aaa671891cc676416ab5a0bbb77b64f3897
cpenha <cpenha@ciandt.com>
2019-10-28 16:05:26 -0300

    Imputando dados nos campos

--- cypress/integration/abrindo-browser.spec.js
+++ cypress/integration/abrindo-browser.spec.js
@@ -1,4 +1,13 @@
 describe ("Tickets", () => {
     beforeEach (() => cy.visit ("https://bit.ly/2XSuwCW"));
-    it ("has 'TICKETBOX' header's heading", () => {});
+        
+    it.only("Preenchendo todos os campos de digitação", () =>{
+        cy.get ("#first-name").type ("Carlos");
+        cy.get ("#last-name").type ("Guillen");
+        cy.get ("#email").type ("cpenha@ciandt.com");
+        cy.get ("#requests").type ("Vegetariano");
+        cy.get ("#signature").type ("Carlos Penha Guillen");
+    });
+
+     it ("has 'TICKETBOX' header's heading", () => {});
 });
