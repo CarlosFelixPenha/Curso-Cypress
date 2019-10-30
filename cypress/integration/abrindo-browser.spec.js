df944cc0775827b997abfb0899f22ac7a3559b83
cpenha <cpenha@ciandt.com>
2019-10-30 14:11:14 -0300

    Primeira atualização no projeto

--- cypress/integration/abrindo-browser.spec.js
+++ cypress/integration/abrindo-browser.spec.js
@@ -1,7 +1,7 @@
 describe ("Tickets", () => {
     beforeEach (() => cy.visit ("https://bit.ly/2XSuwCW"));
         
-    it.only("Preenchendo todos os campos de digitação", () =>{
+    it("Preenchendo todos os campos de digitação", () =>{
         cy.get ("#first-name").type ("Carlos");
         cy.get ("#last-name").type ("Guillen");
         cy.get ("#email").type ("cpenha@ciandt.com");
@@ -9,5 +9,37 @@ describe ("Tickets", () => {
         cy.get ("#signature").type ("Carlos Penha Guillen");
     });
 
-     it ("has 'TICKETBOX' header's heading", () => {});
+    it("Selecione a opção 2", () => {
+        cy.get ("#ticket-quantity") .select ("3");
+    });
+
+    it("Selecione a opção VIP", () => {
+        cy.get ("#vip").check();
+    });
+
+    it("Selecione a opção 'Midia Social'", () => {
+        cy.get ("#social-media").check();
+    });
+
+    it ("Selecione a opção 'Amigo' e 'Publicação' depois desmarcar a opção 'Amigo' ", () => {
+        cy.get ("#friend").check();
+        cy.get ("#publication").check();
+        cy.get ("#friend").uncheck();
+    });
+
+     it("Validar o texto 'TICKETBOX' na tela de Home", () => {
+        cy.get ("header h1").should ("contain", "TICKETBOX");
+     });
+
+     it("Validar campo para email invalido", () => {
+        cy.get ("#email").type ("carlos-teste.com");
+        cy.get ("#email.invalid").should ("exist");
+     });
+
+     it.only("Validar email invalido e depois adicionar um email valido", () => {
+        cy.get ("#email").type ("carlos-teste.com");
+        cy.get ("#email.invalid").should ("exist");
+        cy.get ("#email").clear().type ("carlos@teste.com");
+        cy.get ("#email.invalid").should ("not.exist");
+     });
 });
